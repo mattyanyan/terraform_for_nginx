1994af2a47107517713c99e88c983bb7396dbe33
Matthew Lam <matthewhoyan@gmail.com>
2022-04-15 13:37:04 +0800

    Fetch welcome page content after apply

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
+data "http" "website_content" {
+  url = "http://${module.nginx_instance.public_dns}"
+}
+
 output "nginx_instance_dns" {
   description = "The Public IPv4 DNS of the NGINX instance"
   value       = module.nginx_instance.public_dns
@@ -6,4 +10,9 @@ output "nginx_instance_dns" {
 output "nginx_instance_ip" {
   description = "The Public IPv4 address of the NGINX instance"
   value       = module.nginx_instance.public_ip
+}
+
+output "nginx_welcome_page" {
+  description = "The NGINX welcome page content"
+  value       = data.http.website_content.body
 }
