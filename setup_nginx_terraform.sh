4ea21334f95252e0ffc8cb37c4659e5c8b8761d5
Matthew Lam <matthewhoyan@gmail.com>
2022-04-15 17:10:42 +0800

    Add printing colour in script

--- setup_nginx_terraform.sh
+++ setup_nginx_terraform.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
+print_with_color () {
+    if [ "$1" = "magenta" ] ; then
+        echo -e "\e[1;35m $1 \e[0m"
+    elif [ "$1" = "red" ] ; then
+        echo -e "\e[1;31m $1 \e[0m"
+    elif [ "$1" = "green" ] ; then
+        echo -e "\e[1;32m $1 \e[0m"
+    fi
+}
+
 # Review and apply the infrastucture
-echo -e "Initiate Terraform environment...\n"
+print_with_color "magenta" "Initiate Terraform environment...\n"
 terraform init
 
-echo -e "\nStart applying Terraform infrasturcture...\n"
+print_with_color "magenta" "\nStart applying Terraform infrasturcture...\n"
 terraform apply
 nginx_instance_dns=$(terraform output | grep nginx_instance_dns | awk -F ' = ' '{print $2}' | tr -d '\"')
 if [ -z "$nginx_instance_dns" ] ; then
-    echo "Unable to fetch the NGINX instance DNS"
+    print_with_color "red" "Unable to fetch the NGINX instance DNS"
     exit 1
 fi
 
 # Fetch the NGINX welcome page and count word frequency
-echo -e "\nFetching the NGINX welcome page...\n"
+print_with_color "magenta" "\nFetching the NGINX welcome page...\n"
 curl -s $nginx_instance_dns | xmlstarlet sel -t -v "//body"
-echo -e "\nCounting the word frequency of the welcome page...\n"
+print_with_color "magenta" "\nCounting the word frequency of the welcome page...\n"
 curl -s $nginx_instance_dns | xmlstarlet sel -t -v "//body" | tr '[:punct:]' ' ' | tr ' ' '\n' | awk 'NF' | sort | uniq -c
+
+print_with_color "green" "\nInfrastructure is provisioned\n"
