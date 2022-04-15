7d1b8ba0514d498d3a9660ccb077dfb64b8d6697
Matthew Lam <matthewhoyan@gmail.com>
2022-04-15 16:02:34 +0800

    Remove installing packages in shell script

--- setup_nginx_terraform.sh
+++ setup_nginx_terraform.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Install required packages
-echo "Installed required packages if necessary..."
-sudo apt-get update && sudo apt-get install -y gnupg \
-    software-properties-common \
-    curl \
-    xmlstarlet
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install terraform
-echo ""
-
 # Review and apply the infrastucture
 echo "Initiate Terraform environment..."
 terraform init
