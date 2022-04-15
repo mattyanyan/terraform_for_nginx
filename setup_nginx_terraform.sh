a38181cdfaaba7fe267e07cbd481351d7ef0101e
Matthew Lam <matthewhoyan@gmail.com>
2022-04-15 14:56:53 +0800

    Fix syntax error

--- setup_nginx_terraform.sh
+++ setup_nginx_terraform.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# Install xmlstarlet if it is not installed
-echo "Install xmlstarlet if it is not installed..."
-sudo apt-get update && sudo apt-get -y install xmlstarlet
+# Install required packages
+echo "Installed required packages if necessary..."
+sudo apt-get update && sudo apt-get install -y gnupg \
+    software-properties-common \
+    curl \
+    xmlstarlet
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
 echo ""
 
 # Review and apply the infrastucture
+echo "Initiate Terraform environment..."
+terraform init
 echo "Start applying Terraform infrasturcture..."
 terraform apply
 nginx_instance_dns=$(terraform output | grep nginx_instance_dns | awk -F ' = ' '{print $2}' | tr -d '\"')
-if [ -n $nginx_instance_dns] ; then
+if [ -n $nginx_instance_dns ] ; then
     echo "Unable to fetch the NGINX instance DNS"
     exit 1
 fi
