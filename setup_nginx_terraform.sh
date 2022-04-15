b9b4f8835a33125a516ea36892df0d8a3764578c
Matthew Lam <matthewhoyan@gmail.com>
2022-04-15 14:03:27 +0800

    Add shell script to wrap the actions

---
+++ setup_nginx_terraform.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install xmlstarlet if it is not installed
+echo "Install xmlstarlet if it is not installed..."
+sudo apt-get update && sudo apt-get -y install xmlstarlet
+echo ""
+
+# Review and apply the infrastucture
+echo "Start applying Terraform infrasturcture..."
+terraform apply
+nginx_instance_dns=$(terraform output | grep nginx_instance_dns | awk -F ' = ' '{print $2}' | tr -d '\"')
+if [ -n $nginx_instance_dns] ; then
+    echo "Unable to fetch the NGINX instance DNS"
+    exit 1
+fi
+echo ""
+
+# Fetch the NGINX welcome page and count word frequency
+echo "Fetching the NGINX welcome page..."
+curl -s $nginx_instance_dns | xmlstarlet sel -t -v "//body"
+echo ""
+echo "Counting the word frequency of the welcome page..."
+curl -s $nginx_instance_dns | xmlstarlet sel -t -v "//body" | tr '[:punct:]' ' ' | tr ' ' '\n' | awk 'NF' | sort | uniq -c
